d8cdeab521ff78ff8dd8e8b9baa9e55fca808ea3
Cory Hartford <hartforc@appleton.millsfleetfarm.com>
2017-12-13 09:12:58 -0600

    Changed header and nav bar to coolapse and display on smaller screens. Added hamburger with embedded menu.

--- district02/src/app/d02-components/header-component/desktop-header/desktop-header.component.ts
+++ district02/src/app/d02-components/header-component/desktop-header/desktop-header.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 
 @Component({
@@ -7,9 +8,30 @@ import { Component, Input } from '@angular/core';
     styleUrls: ['./desktop-header.component.css']
 })
 
-export class DesktopHeaderComponent {
+export class DesktopHeaderComponent implements OnInit {
     @Input()
     headerImage;
+    innerWidth = 0;
+    isBig = false;
+
+    ngOnInit() {
+        this.innerWidth = window.innerWidth;
+        this.setScreenSize();
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onresize(event) {
+        this.innerWidth = window.innerWidth;
+        this.setScreenSize();
+    }
+
+
+    setScreenSize() {
+        this.isBig = this.innerWidth > 1199 ? true : false;
+    }
+
+
+
 
 
 }
