76dbdf05fc6459e1b695afea237a0dab8969dbcf
Cory Hartford <cory.hartford.1974@gmail.com>
2018-09-07 13:38:12 -0500

    Adding some new files for Admin

---
+++ district02/src/app/classes/displayed-meeting.ts
@@ -0,0 +1,12 @@
+export class DisplayedMeeting {
+    id: number;
+    city: string;
+    day: string;
+    startTime: Date;
+    name: string;
+    location: string;
+    address: string;
+    type: string;
+    lat: number;
+    lon: number;
+}
---
+++ district02/src/app/d02-components/managers/meeting-management-component/meeting-manager-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector : 'meeting-manager',
+    templateUrl : './meeting-manager-component.html',
+    styleUrls : ['./meeting-manager-component.css']
+})
+
+export class MeetingManagerComponent {
+
+}
